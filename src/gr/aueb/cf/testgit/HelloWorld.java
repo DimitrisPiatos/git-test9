bab6a1402838e62b3b575a6cd34803a9f66ceb8b
dimitris_piatos <mhtsak97@gmail.com>
2025-12-04 17:14:28 +0200

    fix:main fixes

--- src/gr/aueb/cf/testgit/HelloWorld.java
+++ src/gr/aueb/cf/testgit/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("Hello Coding!!!!git");
         System.out.println("Hello Coding");
         System.out.println("Hi everyone");
+        System.out.println("Hello coding 21");
     }
 }
